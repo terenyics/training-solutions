8b9d34a283444ab8aa15c85fdd2403cb9eb1c75e
terenyicsaba <terenyicsaba@gmail.com>
2020-12-04 11:09:55 +0100

    Note metódus commitja

---
+++ src/main/java/classstructuremethods/Note.java
@@ -0,0 +1,38 @@
+package classstructuremethods;
+
+public class Note {
+
+    private String name;
+    private String topic;
+    private String text;
+
+
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getNoteText() {
+        return name + ": " + "(" + topic + ") " + text;
+    }
+
+}
