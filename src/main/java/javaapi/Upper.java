4955066ff047422a28493ff9603ff7214d2e8842
terenyicsaba <terenyicsaba@gmail.com>
2021-01-21 19:08:42 +0100

    javaapi commitja

--- src/main/java/javaapi/Upper.java
+++ src/main/java/javaapi/Upper.java
@@ -2,8 +2,6 @@ package javaapi;
 
 public class Upper {
     public static void main(String[] args) {
-        /*String s = "Hello World!";
-        System.out.println(s.toUpperCase());*/
         System.out.println("Hello World".toUpperCase());
     }
 
