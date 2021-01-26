56935ed4062ffd9d31a797d02f5d37e71574e4f1
terenyicsaba <terenyicsaba@gmail.com>
2021-01-26 19:23:40 +0100

    arrayofarrays commitjai

---
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -0,0 +1,37 @@
+package arrayofarrays;
+
+public class ArrayOfArraysMain {
+
+    public static void main(String[] args) {
+        ArrayOfArraysMain arrayOfArraysMain = new ArrayOfArraysMain();
+        arrayOfArraysMain.printArrayOfArrays(arrayOfArraysMain.multiplicationTable(10));
+    }
+
+    public int[][] multiplicationTable(int size) {
+        int[][] multiplicationTable = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                multiplicationTable[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+        return multiplicationTable;
+    }
+
+    public void printArrayOfArrays(int[][] a) {
+        for (int[] i : a) {
+            for (int j : i) {
+                if (j < 10) {
+                    System.out.print("  ");
+                } else if (j < 100) {
+                    System.out.print(" ");
+                }
+                System.out.print(j);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
